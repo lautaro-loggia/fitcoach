a4fc839c8bbb18b1a064a7b0fc6e0369ac7459fc
Lautaro Loggia <lauloggia@Mac-de-Lau.local>
2025-12-21 02:01:44 -0300

    Fix TrainingTab prop passing clientId

--- src/app/(dashboard)/clients/[id]/page.tsx
+++ src/app/(dashboard)/clients/[id]/page.tsx
@@ -47,7 +47,7 @@ export default async function ClientNotesPage({ params }: { params: { id: string
                     <CheckinTab client={client} />
                 </TabsContent>
                 <TabsContent value="training">
-                    <TrainingTab client={client} />
+                    <TrainingTab clientId={client.id} />
                 </TabsContent>
                 <TabsContent value="diet">
                     <DietTab client={client} />
