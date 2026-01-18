0883e96c4f729d9935bdaabdf0b8dafa6d7a0619
Lautaro Loggia <lauloggia@Mac-de-Lau.local>
2026-01-18 17:44:05 -0300

    fix: radical simplification of database types helpers

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -509,88 +509,36 @@ export type Database = {
 
 
 
+
 type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-    PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-    | { schema: keyof Database } = keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"]),
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? any
-    : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-            Row: infer R
-        }
-    ? R
-    : never
-    : never
+    T extends keyof PublicSchema["Tables"] | { schema: keyof Database } = keyof PublicSchema["Tables"]
+> = T extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][T]["Row"]
+    : any
 
 export type TablesInsert<
-    PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database } = keyof PublicSchema["Tables"],
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? any
-    : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-    }
-    ? I
-    : never
-    : never
+    T extends keyof PublicSchema["Tables"] | { schema: keyof Database } = keyof PublicSchema["Tables"]
+> = T extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][T]["Insert"]
+    : any
 
 export type TablesUpdate<
-    PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database } = keyof PublicSchema["Tables"],
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? any
-    : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-    }
-    ? U
-    : never
-    : never
+    T extends keyof PublicSchema["Tables"] | { schema: keyof Database } = keyof PublicSchema["Tables"]
+> = T extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][T]["Update"]
+    : any
 
 export type Enums<
-    PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database } = keyof PublicSchema["Enums"],
-    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? any
-    : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    T extends keyof PublicSchema["Enums"] | { schema: keyof Database } = keyof PublicSchema["Enums"]
+> = T extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][T]
+    : any
 
 export type CompositeTypes<
-    PublicCompositeTypeNameOrOptions extends
-    | keyof PublicSchema["CompositeTypes"]
-    | { schema: keyof Database } = keyof PublicSchema["CompositeTypes"],
-    CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-        schema: keyof Database
-    }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-    ? any
-    : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    T extends keyof PublicSchema["CompositeTypes"] | { schema: keyof Database } = keyof PublicSchema["CompositeTypes"]
+> = T extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][T]
+    : any
 
